be7574377c9e01ee2eab1f1ac6f0f9d051005e15
Siam Khushnobish <siam@zaagsys.com>
2024-07-04 13:02:16 +0600

    add multiple deletion feature

--- roles/urls.py
+++ roles/urls.py
@@ -9,6 +9,4 @@ router.register(r'roles', RoleViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('roles/delete-multiple/', RoleViewSet.as_view({'delete': 'delete_multiple'}), name='role-delete-multiple'),
-    # http://127.0.0.1:8000/api/roles/delete-multiple/?ids=ef7d579e-dafa-4000-a384-82e3e677fdc1,60b8e54e-a942-43b5-9dfd-9d7e3de6ec8b
 ]
--- roles/views.py
+++ roles/views.py
@@ -30,7 +30,7 @@ class PermissionViewSet(SuccessMessageMixin, viewsets.ModelViewSet):
 
 class RoleViewSet(SuccessMessageMixin, viewsets.ModelViewSet):
     """
-    ViewSet for handling CRUD operations for Role, including bulk deletion.
+    ViewSet for handling CRUD operations for Role, including multiple deletion.
     """
 
     queryset = Role.objects.all()
@@ -38,3 +38,23 @@ class RoleViewSet(SuccessMessageMixin, viewsets.ModelViewSet):
     filter_backends = [SearchFilter]  # Add search filter
     search_fields = ['name']
     # permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['delete'], url_path='delete-multiple')
+    def delete_multiple(self, request):
+        """
+        Custom action to handle multiple deletion of roles.
+        This action expects a list of role IDs as query parameters.
+        Example request: DELETE /api/roles/delete-multiple/?ids=uuid1,uuid2,uuid3
+        """
+        ids = request.query_params.get('ids')
+        if not ids:
+            return Response({"message": "No IDs provided."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        ids_list = ids.split(',')
+        roles = Role.objects.filter(id__in=ids_list)
+        count, _ = roles.delete()
+        
+        if count == 0:
+            return Response({"message": "No roles found for the provided IDs."}, status=status.HTTP_404_NOT_FOUND)
+        
+        return Response({"message": f"Roles deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
