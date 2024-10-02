57b5f5d468d3ab142349fa9350c3b6074809f4cd
Siam Khushnobish <siam@zaagsys.com>
2024-10-02 13:54:54 +0600

    order_number field add in order module

--- orders/models.py
+++ orders/models.py
@@ -5,6 +5,9 @@ from common.model import BaseModel
 from products.models import ProductVariant
 from coupons.models import Coupon
 from address.models import Address
+import time
+import random
+import string
 
 class ShippingMethod(BaseModel):
     name = models.CharField(max_length=255)
@@ -16,7 +19,25 @@ class ShippingMethod(BaseModel):
         return self.name
 
 class Order(BaseModel):
+    PAYMENT_METHOD_CHOICES = [
+        ('cash_on_delivery', 'Cash on Delivery'), 
+        ('stripe', 'Stripe')
+    ]
+    
+    PAYMENT_STATUS_CHOICES = [
+        ('pending', 'Pending'), 
+        ('paid', 'Paid'),
+        ('failed', 'Failed'),
+    ]
+    ORDER_STATUS_CHOICES = [
+            ('processing', 'Processing'), 
+            ('shipped', 'Shipped'), 
+            ('delivered', 'Delivered'), 
+            ('cancelled', 'Cancelled')
+    ]
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=20, unique=True, null=True, blank=True, editable=False)
     billing_address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, related_name='billing_orders')
     shipping_address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, related_name='shipping_orders')
     shipping_method = models.ForeignKey(ShippingMethod, on_delete=models.SET_NULL, null=True)
@@ -28,15 +49,22 @@ class Order(BaseModel):
     total = models.DecimalField(max_digits=10, decimal_places=2)
     price_before_discount = models.DecimalField(max_digits=10, decimal_places=2)
     is_coupon_applied = models.BooleanField(default=False)
-    payment_method = models.CharField(max_length=50, choices=[('cash_on_delivery', 'Cash on Delivery'), ('stripe', 'Stripe')])
-    payment_status = models.CharField(max_length=50, choices=[('pending', 'Pending'), ('paid', 'Paid')], default='pending')
-    order_status = models.CharField(max_length=50, choices=[('processing', 'Processing'), ('shipped', 'Shipped'), ('delivered', 'Delivered'), ('cancelled', 'Cancelled')], default='processing')
+    payment_method = models.CharField(max_length=50, choices=PAYMENT_METHOD_CHOICES)
+    payment_status = models.CharField(max_length=50, choices=PAYMENT_STATUS_CHOICES, default='pending')
+    order_status = models.CharField(max_length=50, choices=ORDER_STATUS_CHOICES, default='processing')
     active = models.BooleanField(default=True)
+
+    def generate_unique_order_number(self):
+        timestamp = int(time.time())
+        random_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+        return f"ORD-{timestamp}-{random_str}"
 
     def __str__(self):
         return f"Order({self.id}, {self.user})"
     
     def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self.generate_unique_order_number()
         if self.order_status == 'delivered' or self.order_status == 'cancelled':
             self.active = False
         super(Order, self).save(*args, **kwargs)
