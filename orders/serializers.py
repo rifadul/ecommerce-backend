d7e160bd4c84cb6b5135eabdbcbd59803e47de1a
Siam Khushnobish <siam@zaagsys.com>
2024-10-02 15:08:29 +0600

    change fields order in  OrderSerializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -30,7 +30,7 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['id','order_number', 'active', 'billing_address', 'shipping_address', 'shipping_method', 'coupon', 'subtotal', 'tax', 'shipping', 'price_before_discount', 'discount', 'total', 'is_coupon_applied', 'payment_method', 'payment_status', 'order_status', 'items']
+        fields = ['id','order_number', 'active', 'coupon', 'subtotal', 'tax', 'shipping', 'price_before_discount', 'discount', 'total', 'is_coupon_applied', 'payment_method', 'payment_status', 'order_status','billing_address', 'shipping_address', 'shipping_method', 'items']
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
